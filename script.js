24b2a9f1bf620f90fee48fabae0899c2c0466759
Gabriel Richter Reynoso <garey91@gmail.com>
2024-09-14 22:57:23 +0200

    changedSizeOnNavBtn

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const menuButton = document.querySelector('.menubtn');
-const closeMenuButton = document.querySelector('.closebtn');
+const navBtn = document.getElementById('navBtn');
 
-menuButton.addEventListener('click', () =>{
-    closeMenuButton.style.display = 'block';
-    menuButton.style.display = 'none';
-})
-
-closeMenuButton.addEventListener('click', () =>{
-    closeMenuButton.style.display = 'none';
-    menuButton.style.display = 'block';
+navBtn.addEventListener('click', () =>{
+    navBtn.classList.toggle('fa-xmark');
 })
